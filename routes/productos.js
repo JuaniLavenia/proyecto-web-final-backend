735da116920c33b26761938f9221cbe57bf26a7e
Dario Alba <88122951+Darioalba@users.noreply.github.com>
2023-05-12 13:12:07 -0300

    se corrige ruta

--- routes/productos.js
+++ routes/productos.js
@@ -5,16 +5,16 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const multer = require("multer");
 
-//creamos el storage para guardar la imagen
-const storage = multer.diskStorage({ //storage de disco
+
+const storage = multer.diskStorage({ 
   destination: function (req, file, cb) {
-    cb(null, "./public/img/productos"); //ubicacion donde se va a guarda
+    cb(null, "./public/img/productos"); 
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname); //nombre original del archivo
+    cb(null, Date.now() + "-" + file.originalname); 
   },
 });
-//este va a subir el archivo
+
 const upload = multer({ storage: storage });
 
 //rutas
@@ -35,27 +35,24 @@ router.get("/productos", async (req, res) => {
   }
 });
 
-//vamos a traer un solo producto, usa findById
+
 router.get("/productos/:id", async (req, res) => {
   try {
-    //aqui busco los datos dentro de la base
-    //esto es una promesa y se trae por el ID
+    
     const producto = await Producto.findById(req.params.id);
     console.log(producto);
-    // estos datos ya viene de la base de datos
+  
     res.json(producto);
   } catch (err) {
     console.log(err);
   }
 });
 
-//vamos a crear un solo producto
-//upload sigle sube una sola imagen
 router.post("/productos", upload.single("image"), async (req, res) => {
   console.log(req.body, req.file);
   
   try {
-    //creamos un product
+  
     const producto = new Producto({
       name: req.body.name,
       description: req.body.description,
@@ -66,20 +63,19 @@ router.post("/productos", upload.single("image"), async (req, res) => {
       category: req.body.category,
       
     });
-    //guardamos
+
     const result = await producto.save();
-    // console.log(result)
+  
     res.json(result);
   } catch (err) {
     console.log(err);
   }
 });
 
-//vamos a actualizar un registro
 router.put("/productos/:id", upload.single("image"), async (req, res) => {
-  // console.log(req.body)
+ 
   try {
-    //modifica un parametro por ID
+   
     const result = await Producto.findByIdAndUpdate(
       req.params.id, //aqui lo va a buscar
       {
@@ -94,28 +90,24 @@ router.put("/productos/:id", upload.single("image"), async (req, res) => {
       {
         new: true,
       }
-    ); //trae el registro que acaba de crear
+    ); 
     res.json(result);
   } catch (err) {
     console.log(err);
   }
 });
 
-
-//vamos a borrar un registro
 router.delete("/productos/:id", async (req, res) => {
-  // console.log(req.body)
-  try {
-    //busca y brorra el elemento
+ 
+  try {  
     const result = await Producto.findByIdAndDelete(req.params.id);
     const msg = result ? "Registro borrado" : "No se encontro el registro";
-    res.json({ msg }); //me trae el registro que borro
+    res.json({ msg }); 
   } catch (err) {
     console.log(err);
   }
 });
 
-//Ruta de buscador
 router.get("/productos/search/:filter", async (req, res) => {
   const { filter } = req.params
   try {
@@ -127,7 +119,7 @@ router.get("/productos/search/:filter", async (req, res) => {
   }
 });
 
-//Ruta de categoria
+
 router.get("/productos/category/:filter", async (req, res) => {
   const { filter } = req.params;
 
